f30542317ab67b0884496ed49012e87a839d133c
KAEDET <kaede88@gmail.com>
2016-09-17 13:40:51 +0900

    辺追加後のASPLの一般形の追加

--- nd2_verify.py
+++ nd2_verify.py
@@ -12,10 +12,16 @@ if __name__ == '__main__':
 
     G = nx.read_edgelist('results/n' + str(q**2) + 'd' + str(q) +
                          'or' + str(q - 1) + '_edgelist.txt')
+    H = nx.read_edgelist('results/n' + str(q**2) + 'd' + str(q) +
+                          '_edgelist.txt')
 
-    predicted_aspl = 2 * (q**2 + q - 1) / (q**2 + q)
-    aspl = nx.average_shortest_path_length(G)
+    predicted_aspl_before_adding_edges = 2 * (q**2 + q - 1) / (q**2 + q)
+    predicted_aspl_after_adding_edges = 2 * (q**3 + q**2 - q - 2) / (q**3 + q**2)
+    aspl_before_adding_edges = nx.average_shortest_path_length(G)
+    aspl_after_adding_edges = nx.average_shortest_path_length(H)
 
-    assert predicted_aspl == aspl
-    print("predicted_aspl:", predicted_aspl)
-    print("aspl:", aspl)
+    #assert predicted_aspl == aspl
+    print("predicted_aspl_before_adding_edges:", predicted_aspl_before_adding_edges)
+    print("predicted_aspl_after_adding_edges:", predicted_aspl_after_adding_edges)
+    print("aspl_before_adding_edges:", aspl_before_adding_edges)
+    print("aspl_after_adding_edges:", aspl_after_adding_edges)
